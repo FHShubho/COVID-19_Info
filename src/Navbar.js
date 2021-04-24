77a65c28e634b3c699d119fbce6b62c11587f32b
Fahimul Hoque <fhshubho@gmail.com>
2021-04-24 21:24:14 +0600

    Add files via upload

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+import './Navbar.css';
+
+const Navbar = () => {
+    return(
+        <nav className="navbar">
+            <h1>COVID-19 Info</h1>
+        </nav>
+
+    );
+}
+
+export default Navbar;
