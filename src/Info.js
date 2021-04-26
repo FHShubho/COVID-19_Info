8e5f491175740a150951ef68032d78df093b13f0
Fahimul Hoque <fhshubho@gmail.com>
2021-04-26 21:57:31 +0600

    situation summery

--- src/Info.js
+++ src/Info.js
@@ -6,85 +6,117 @@ import confirmed_logo from './images/confirmed.png'
 import recovered_logo from './images/recovered.png'
 import isolated_logo from './images/isolated.png'
 import './Info.css'
+import loading_logo from './images/loading.svg'
+import { useState, useEffect } from 'react'
 
 const Info = () => {
-    return(
-        <div className="container-fluid info-custom">
-            <div className="row">
-                <div className="col-6 d-flex justify-content-center ps-5">
-                    <img src={logo2} alt="" width="100%" height="auto"/>
+
+    const [all_data, setAllData] = useState(null); 
+
+    useEffect(() => {
+        fetch('https://shubho-covid-info-api.herokuapp.com/api/summery')
+            .then(rest => {
+                return rest.json();
+            })
+            .then(data => {
+                console.log(data);
+                setAllData(data);
+            })
+    }, []);
+
+    if(all_data == null) {
+        return(
+            <div className="loading">
+                <img src={loading_logo} alt=""/>
+                loading latest data
+            </div>
+        );
+    }
+    else {
+        return(
+            <div className="container-fluid info-custom">
+                <div className="row">
+                    <div className="col-6 d-flex justify-content-center ps-5">
+                        <img src={logo2} alt="" width="100%" height="auto"/>
+                    </div>
+                    <div className="col-6 d-flex justify-content-center">
+                        <div className="row gy-0">
+                            <div className="col-12">
+                            </div>                   
+                            <div className="col-12 d-flex justify-content-center align-items-center">                           
+                                {/* <img src={time_logo} alt="" width="40" height="40"/> */}
+                                <h2 className="ms-1 header-custom">Last 24 Hours</h2>
+                                <br/>
+                            </div>
+                            <div className="col-12 d-flex justify-content-center align-items-center">
+                                <img className="" src={death_logo} alt="" width="50" height="auto"/>
+                                <h1 className="ms-1 info-text-custom">{all_data.latest_death} Deaths</h1>
+                            </div>
+                            <div className="col-6 d-flex justify-content-center align-items-center">
+                                <img className="" src={test_logo} alt="" width="30" height="35"/>
+                                <h2 className="ms-1 info-text-custom">{all_data.latest_lab_test} Lab Tests</h2>
+                            </div>
+                            <div className="col-6 d-flex justify-content-center align-items-center">
+                                <img className="" src={confirmed_logo} alt="" width="40" height="40"/>
+                                <h2 className="ms-1 info-text-custom">{all_data.latest_confirmed_case} Confirmed</h2>
+                            </div>
+                            <div className="col-6 d-flex justify-content-center align-items-center">
+                                <img className="" src={recovered_logo} alt="" width="40" height="40"/>
+                                <h2 className="ms-1 info-text-custom">{all_data.latest_recoverd} Recovered</h2>
+                            </div>
+                            <div className="col-6 d-flex justify-content-center align-items-center">
+                                <img className="" src={isolated_logo} alt="" width="30" height="30"/>
+                                <h2 className="ms-1 info-text-custom">{all_data.latest_isolated} Isolated</h2>
+                            </div>
+                            <div className="col-12">
+                            </div>
+                            <div className="col-12">
+                            </div>  
+                        </div>
+                    </div>
                 </div>
-                <div className="col-6 d-flex justify-content-center">
-                    <div className="row">                   
-                        <div className="col-12 d-flex justify-content-center align-items-center pt-5">                           
-                            <img src={time_logo} alt="" width="40" height="40"/>
-                            <h2 className="ms-1 header-custom">Last 24 Hours</h2>
-                            <br/>
+                
+                <div className="row row-cols-6 info-box-custom mt-lg-4 ms-1 me-1">
+                    <div className="col">
+                        <div className="alert alert-primary border-start-5 border-end-0 border-top-0 border-bottom-0 border-5 rounded-3">
+                            <h3 className="alert-heading">{all_data.total_lab_test}</h3>
+                            Total Lab Test
                         </div>
-                        <div className="col-12 d-flex justify-content-center align-items-center">
-                            <img className="" src={death_logo} alt="" width="50" height="auto"/>
-                            <h1 className="ms-1 info-text-custom">83 Deaths</h1>
+                    </div>
+                    <div className="col">
+                        <div className="alert alert-warning border-start-5 border-end-0 border-top-0 border-bottom-0 border-5 rounded-3">
+                            <h3 className="alert-heading">{all_data.total_confirmed_case}</h3>
+                            Total Confirmed
                         </div>
-                        <div className="col-6 d-flex justify-content-center align-items-center">
-                            <img className="" src={test_logo} alt="" width="30" height="30"/>
-                            <h2 className="ms-1 info-text-custom">20000 Lab Tests</h2>
+                    </div>
+                    <div className="col">
+                        <div className="alert alert-secondary border-start-5 border-end-0 border-top-0 border-bottom-0 border-5 rounded-3">
+                            <h3 className="alert-heading">{all_data.total_isolated}</h3>
+                            Total Isolated
                         </div>
-                        <div className="col-6 d-flex justify-content-center align-items-center">
-                            <img className="" src={confirmed_logo} alt="" width="40" height="40"/>
-                            <h2 className="ms-1 info-text-custom">5000 Confirmed</h2>
+                    </div>
+                    <div className="col">
+                        <div className="alert alert-success border-start-5 border-end-0 border-top-0 border-bottom-0 border-5 rounded-3">
+                            <h3 className="alert-heading">{all_data.total_recovered}</h3>
+                            Total Recovered
                         </div>
-                        <div className="col-6 d-flex justify-content-center align-items-center">
-                            <img className="" src={recovered_logo} alt="" width="40" height="40"/>
-                            <h2 className="ms-1 info-text-custom">2000 Recovered</h2>
+                    </div>
+                    <div className="col">
+                        <div className="alert alert-danger border-start-5 border-end-0 border-top-0 border-bottom-0 border-5 rounded-3">
+                            <h3 className="alert-heading">{all_data.total_death}</h3>
+                            Total Death
                         </div>
-                        <div className="col-6 d-flex justify-content-center align-items-center">
-                            <img className="" src={isolated_logo} alt="" width="30" height="30"/>
-                            <h2 className="ms-1 info-text-custom">3000 Isolated</h2>
+                    </div>
+                    <div className="col">
+                        <div className="alert alert-info border-start-5 border-end-0 border-top-0 border-bottom-0 border-5 rounded-3">
+                            <h3 className="alert-heading">{all_data.total_vaccine}</h3>
+                            Total Vaccinated
                         </div>
                     </div>
                 </div>
             </div>
-            
-            <div class="row row-cols-6 info-box-custom mt-lg-4 ms-1 me-1">
-                <div class="col">
-                    <div className="alert alert-primary border-start-5 border-end-0 border-top-0 border-bottom-0 border-5 rounded-3">
-                        <h3 className="alert-heading">1111111111</h3>
-                        Total Lab Test
-                    </div>
-                </div>
-                <div class="col">
-                    <div className="alert alert-warning border-start-5 border-end-0 border-top-0 border-bottom-0 border-5 rounded-3">
-                        <h3 className="alert-heading">1111111111</h3>
-                        Total Confirmed
-                    </div>
-                </div>
-                <div class="col">
-                    <div className="alert alert-secondary border-start-5 border-end-0 border-top-0 border-bottom-0 border-5 rounded-3">
-                        <h3 className="alert-heading">1111111111</h3>
-                        Total Isolated
-                    </div>
-                </div>
-                <div class="col">
-                    <div className="alert alert-success border-start-5 border-end-0 border-top-0 border-bottom-0 border-5 rounded-3">
-                        <h3 className="alert-heading">1111111111</h3>
-                        Total Recovered
-                    </div>
-                </div>
-                <div class="col">
-                    <div className="alert alert-danger border-start-5 border-end-0 border-top-0 border-bottom-0 border-5 rounded-3">
-                        <h3 className="alert-heading">1111111111</h3>
-                        Total Death
-                    </div>
-                </div>
-                <div class="col">
-                    <div className="alert alert-info border-start-5 border-end-0 border-top-0 border-bottom-0 border-5 rounded-3">
-                        <h3 className="alert-heading">1111111111</h3>
-                        Total Vaccinated
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Info;
