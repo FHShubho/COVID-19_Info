c84eacf1384fae7970707251e77e0f1bf2f43e5f
Fahimul Hoque <fhshubho@gmail.com>
2021-04-26 21:58:39 +0600

    hospital info linked

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,12 +5,12 @@ const Navbar = () => {
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid custom-nav-container">
-                <a class="navbar-brand" href="#">
-                    <img src={logo} alt="" width="50px" height="50px" class="d-inline-block"/>
-                    COVID-19 Info
+                <a className="navbar-brand" href="/">
+                    <img src={logo} alt="" width="50px" height="50px" className="d-inline-block"/>
+                    COVID-19 Info BD
                 </a>
                 <div className="justify-content-end nav-item-custom">
-                    <a className="nav-link me-2" href="#">Github</a>
+                    <a className="nav-link me-2" href="#hospital">Hospital Info</a>
                 </div>
             </div>
         </nav>
