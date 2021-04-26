ebdca1a0fefd45a972c3e55a198258a74daa3774
Fahimul Hoque <fhshubho@gmail.com>
2021-04-26 20:59:58 +0600

    added personal website and github repo link

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,16 @@
+import github_logo from './images/github.png'
+
 const Footer = () => {
     return(
         <div className="footer">
-            <div className="container-fluid mt-4 text-end">
-                Data Source: DHGS
+            <div className="container-fluid mt-4 text-end"> 
+                <div className="card bg-secondary pb-0">
+                    <div className="card-header fs-5">
+                        <pre className="mb-0 my-auto"> 
+                            <a href="https://fhshubho.netlify.app/" style={{textDecoration:"none", color:"white"}}>Fahimul Hoque Shubho</a> | <a href="https://github.com/FHShubho/COVID-19_Info" style={{textDecoration:"none", color:"white"}}><img src={github_logo} alt="" width="25px" height="25px"/> Github</a> 
+                         </pre>
+                    </div>
+                </div>
             </div>
         </div>
     );
